f91c9a93dbd1853f7134d3221747e6bbd492011b
JohnDan72 <mr._dany@hotmail.com>
2021-10-24 23:33:03 -0500

    Cambio html page 404 not found

--- src/app/notpagefound/notpagefound.component.ts
+++ src/app/notpagefound/notpagefound.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styleUrls: [ './notpagefound.component.css' ]
+  styleUrls: ['./notpagefound.component.css']
 })
 export class NotpagefoundComponent {
 
   year = new Date().getFullYear();
 
+  constructor() {
+    document.querySelector('html').className = "allHeighWidth";
+    document.body.className = "allHeighWidth";
+  }
+
+  ngOnDestroy() {
+    document.querySelector('html').className = "";
+    document.body.className = "";
+  }
 }
